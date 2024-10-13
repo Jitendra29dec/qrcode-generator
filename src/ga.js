b5203a79813356048e146fa92c627f8f0275e4de
Jitendra <jitendra29dec@gmail.com>
2024-10-13 21:43:10 +0530

    add

--- src/ga.js
+++ src/ga.js
@@ -1,7 +1,7 @@
 // src/ga.js
 import ReactGA from 'react-ga4';
 
-const measurementId = "G-KNSZY2W3KB"; // Replace with your GA4 Measurement ID
+const measurementId = "G-ABCDEFGHI"; // Replace with your GA4 Measurement ID
 ReactGA.initialize(measurementId);
 
 export const trackPage = (page) => {
